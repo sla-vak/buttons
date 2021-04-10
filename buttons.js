a4a0c3b517a857976b6c7f141e3163c06226a15f
sliva <64837201+sla-vak@users.noreply.github.com>
2021-04-10 13:49:27 +0300

    new site

---
+++ buttons.js
@@ -0,0 +1,50 @@
+let b1 = document.querySelector('#b1');
+b1.onclick = function () {
+    document.getElementById('b2').style.display = 'block';
+}
+
+let b2 = document.querySelector('#b2');
+b2.onclick = function () {
+    document.getElementById('b3').style.display = 'block';
+}
+
+let b3 = document.querySelector('#b3');
+b3.onclick = function () {
+    document.getElementById('b4').style.display = 'block';
+}
+
+let b4 = document.querySelector('#b4');
+b4.onclick = function () {
+    document.getElementById('b5').style.display = 'block';
+}
+
+let b5 = document.querySelector('#b5');
+b5.onclick = function () {
+    document.getElementById('b6').style.display = 'block';
+}
+
+let b6 = document.querySelector('#b6');
+b6.onclick = function () {
+    document.getElementById('b7').style.display = 'block';
+}
+
+let b7 = document.querySelector('#b7');
+b7.onclick = function () {
+    document.getElementById('b8').style.display = 'block';
+}
+
+let b8 = document.querySelector('#b8');
+b8.onclick = function () {
+    document.getElementById('b9').style.display = 'block';
+}
+
+let b9 = document.querySelector('#b9');
+b9.onclick = function () {
+    document.getElementById('b10').style.display = 'block';
+}
+
+let b10 = document.querySelector('#b10');
+b10.onclick = function () {
+    alert('okay you win!');
+    window.location.reload();
+}
